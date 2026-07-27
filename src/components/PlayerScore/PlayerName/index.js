62942909f5b6a29f4be2d16407b171f3e63f0525
Anna Ramezoni <anna.ramezoni@gmail.com>
2026-07-27 17:11:02 +1200

    Creat PlayerName component without CSS

---
+++ src/components/PlayerScore/PlayerName/index.js
@@ -0,0 +1,6 @@
+function PlayerName(content) {
+  return `
+    <p>${content}</p>
+    `;
+}
+export default PlayerName;
--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@ import "/src/components/styles/generic/reset.css"; //reset
 import "/src/components/styles/elements/base.css"; //general css styles
 
 import BoardGame from "./objects/BoardGame";
+import PlayerName from "./components/PlayerScore/PlayerName";
 
 const $root = document.querySelector("#root ");
-const $htmlBoardGame = BoardGame(6);
 
-$root.insertAdjacentHTML("beforeend", $htmlBoardGame);
+$root.insertAdjacentHTML(
+  "beforeend",
+  `
+    ${PlayerName("Player1")}
+    ${PlayerName("Player2")}
+    ${BoardGame(6)}
+    `,
+);
