9f1898dae8d9bebc05e52fa1873050ef96e39303
Anna Ramezoni <anna.ramezoni@gmail.com>
2026-08-18 18:25:21 +1200

    Create JS, HTML and CSS cards

---
+++ src/objects/BoardGame/data.js
@@ -0,0 +1,28 @@
+const cards = [
+  {
+    icon: "javascript",
+    altIcon: "JavaScript logo",
+  },
+  {
+    icon: "javascript",
+    altIcon: "JavaScript logo",
+  },
+  {
+    icon: "html",
+    altIcon: "HTML logo",
+  },
+  {
+    icon: "html",
+    altIcon: "HTML logo",
+  },
+  {
+    icon: "css",
+    altIcon: "CSS logo",
+  },
+  {
+    icon: "css",
+    altIcon: "CSS logo",
+  },
+];
+
+export default cards;
